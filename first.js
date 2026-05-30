2ebb6861decc5221e0b7ebd0ca12dc2fd4caad8d
yashmahindroo18 <yashmahindroo10@gmail.com>
2026-05-30 16:38:32 +0530

    learned about variable types

--- first.js
+++ first.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+const student ={
+    name : "yash",
+    age : 20,
+    CGPA:9.1,
+};
+student["name"]="rahul";
+console.log(student.age);
+console.log(student["CGPA"]);
+console.log(student["name"]); 
