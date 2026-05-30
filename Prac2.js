0c6fb3626868650ce010490d6625a9d469a99667
yashmahindroo18 <yashmahindroo10@gmail.com>
2026-05-30 16:48:14 +0530

    second practice

---
+++ Prac2.js
@@ -0,0 +1,12 @@
+//  insta page 
+const profile={
+    name=shadhakhapra,
+    follow=false,
+    message=false,
+    posts=195,
+    followers=262012,
+    following =4,
+    username="Sharadha khapra",
+    organisation : "Apna college",
+    promo:"to educate ",
+}
