134b2d3fe67143f190c6881958856f65a9deb0f2
yashmahindroo18 <yashmahindroo10@gmail.com>
2026-05-30 16:43:00 +0530

    cover page using an object

---
+++ Prac1.js
@@ -0,0 +1,9 @@
+// amazon pen example 
+//code an object for the image shown 
+const amazon={
+    name:"Parker Jotter Standard CT Ball Pen",
+    rating : 7002,
+    offer:5,
+    price:270,
+}
+console.log(amazon["name"]);
