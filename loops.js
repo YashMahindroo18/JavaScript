054540e54696a819f15b4b84e5320adfc1dd3f4e
yashmahindroo18 <yashmahindroo10@gmail.com>
2026-06-01 13:26:28 +0530

    entered a new loop format

---
+++ loops.js
@@ -0,0 +1,33 @@
+// let i=0;
+// for(i;i<10;i++){
+//     console.log("hello world!")
+// }
+// // calculate sum of first n numbers 
+// let sum =0;
+// for(let j=1;j<=5;j++){
+//     sum+=j;
+// }
+// let str="JavaScript";
+// let size=0;
+// for(let i of str){
+//     console.log("i=",i);
+//     size++;
+// }
+// console.log(size);
+// console.log(sum);
+let details={
+    name:"yash",
+    age:20,
+    pass:true,
+    cgpa:true,
+}
+// for(let i in details){
+//     console.log(i);
+// }
+let n=100;
+for(let i=0;i<=n;i++){
+    if(i%2==0){
+        console.log(i);
+    }
+}
+console.log("Finished");
